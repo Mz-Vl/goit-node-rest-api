4bf9a89e96b825dec472629f1e06329bec6e965a
Vlad Mazur <vladopermoz@gmail.com>
2025-03-07 22:03:43 +0200

    Add avatars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
@@ -11,6 +12,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("/avatars", express.static(path.resolve("public/avatars")));
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,6 +3,7 @@ import validateBody from "../helpers/validateBody.js";
 import { authSchema } from "../schemas/authSchemas.js";
 import { register, login, logout, getCurrentUser } from "../controllers/authController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { updateAvatar, upload } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
@@ -10,5 +11,6 @@ authRouter.post("/register", validateBody(authSchema), register);
 authRouter.post("/login", validateBody(authSchema), login);
 authRouter.post("/logout", authMiddleware, logout);
 authRouter.get("/current", authMiddleware, getCurrentUser);
+authRouter.patch("/avatars", authMiddleware, upload.single("avatar"), updateAvatar);
 
 export default authRouter;
